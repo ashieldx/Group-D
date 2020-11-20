6af853597810db805420296a875c7f34405cca31
Ryne Ferdinand <ryne,ferdinand@binus.ac.id>
2020-11-20 22:23:44 +0700

    Linked List
    - Add order (dessert sama drink)
    - View menu
    - buat struct history

--- Project.cpp
+++ Project.cpp
@@ -5,12 +5,45 @@
 #include <unistd.h>
 #include "Cookingprocess.h"
 
+struct Food{
+	char type[10];
+	char name[205];
+	char flavor[25];
+	char topping[25];
+	double calories;
+	int price;
+	char size;
+	struct Food *next, *prev;
+} *foodHead = NULL, *foodTail = NULL, *foodCurr = NULL;
+
+struct History{
+	char type[10];
+	char name[205];
+	char flavor[25];
+	char topping[25];
+	double calories;
+    int price;
+    char size;
+    int time;
+	int day;
+	int month;
+	int year;
+	int hours;
+	int minute;
+	int second;
+	char time_stat[3];
+	struct History *next, *prev;
+} *historyHead = NULL, *historyCurr = NULL, *historyTail = NULL;
+
 void menu();
 void add();
 void end();
 void sleep();
 void cls();
 void history();
+struct Food *createFood(char type[], char name[], char flavor[], char topping[], double calories, int price, char size);
+void pushFoodTail(char type[], char name[], char flavor[], char topping[], double calories, int price, char size);
+void lowerCase(char str[]);
 
 
 int main(){
@@ -20,8 +53,11 @@ return 0;
 
 void addDessert(){
 	cls();
+	char tempname[200];
+	int price;
+	char topping[25];
+	double cal;
 	while(1){
-		char tempname[200];
 		printf("Input the name [at least 5 characters]: ");
 		scanf("%[^\n]", tempname);
 		getchar();
@@ -29,12 +65,10 @@ void addDessert(){
 		if(len<5){
 			continue;
 		}else{
-			strcpy(food[x].name, tempname);
 			break;
 		}
 	}
 	while(1){
-		int price;
 		printf("Input the price [10-500]: $ ");
 		scanf("%d", &price);
 		getchar();
@@ -46,37 +80,34 @@ void addDessert(){
 		}
 	}
 	while(1){
-		char topping[25];
 		printf("Input the topping ['Caramel' | 'Honey' | 'Syrup'](Case Insensitive): ");
 		scanf("%[^\n]", topping);
 		getchar();
-		if(strcmp(topping,"honey") == 0 || strcmp(topping,"Honey") == 0){
-			strcpy(food[x].topping, "Honey");
+		lowerCase(topping);
+		if(strcmp(topping,"honey") == 0){
+			topping[0] = topping[0] - 'a' + 'A';
 			break;
-		}else if(strcmp(topping,"caramel") == 0 || strcmp(topping,"Caramel") == 0){
-			strcpy(food[x].topping, "Caramel");
+		}else if(strcmp(topping,"caramel") == 0){
+			topping[0] = topping[0] - 'a' + 'A';
 			break;
-		}else if(strcmp(topping,"syrup") == 0 || strcmp(topping,"Syrup") == 0){
-			strcpy(food[x].topping, "Syrup");
+		}else if(strcmp(topping,"syrup") == 0){
+			topping[0] = topping[0] - 'a' + 'A';
 			break;
 		}else{
 			continue;
 		}
 	}
 	while(1){
-		double cal;
 		printf("Insert calories [1.00 - 99.00]: ");
 		scanf("%lf", &cal);
 		getchar();
 		if(cal < 1.00 || cal > 99.00){
 			continue;
 		}else{
-			food[x].calories = cal;
 			break;
 		}
 	}
-	strcpy(food[x].flavor,"-");
-	food[x].size = '-';
+	pushFoodTail("", tempname, "-", topping, cal, price, '-');
 	puts("\nSuccesfully added a new menu!");
 	x++;		
 	char enter;
@@ -86,8 +117,11 @@ void addDessert(){
 
 void addDrink(){
 	cls();
+	char tempname[200];
+	int price;
+	char flavor[25];
+	char size;
 	while(1){
-		char tempname[200];
 		printf("Input the name [at least 5 characters]: ");
 		scanf("%[^\n]", tempname);
 		getchar();
@@ -95,61 +129,48 @@ void addDrink(){
 		if(len<5){
 			continue;
 		}else{
-			strcpy(food[x].name, tempname);
 			break;
 		}
 	}
 	while(1){
-		int price;
 		printf("Input the price [10-500]: $ ");
 		scanf("%d", &price);
 		getchar();
 		if(price<10 || price > 500){
 			continue;
 		}else{
-			food[x].price = price;
 			break;
 		}
 	}
 	while(1){
-		char flavor[25];
 		printf("Input the flavor ['Mint' | 'Mix Berry' | 'Cheese'](Case Sensitive): ");
 		scanf("%[^\n]", flavor);
 		getchar();
 		if(strcmp(flavor,"Mint") == 0){
-			strcpy(food[x].flavor, "Mint");
 			break;
 		}else if(strcmp(flavor,"Mix Berry") == 0){
-			strcpy(food[x].flavor,"Mix Berry");
 			break;
 		}else if(strcmp(flavor,"Cheese") == 0){
-			strcpy(food[x].flavor,"Cheese");
 			break;
 		}else{
 			continue;
 		}
 	}
 	while(1){
-		char size;
 		printf("Input the size [S | M | L](Case Sensitive): ");
 		scanf("%c", &size);
 		getchar();
 		if(size == 'S'){
-			food[x].size = 'S';
 			break;
 		}else if(size == 'M'){
-			food[x].size = 'M';
 			break;
 		}else if(size == 'L'){
-			food[x].size = 'L';
 			break;
 		}else{
 			continue;
 		}
 	}
-	
-	strcpy(food[x].topping, "-");
-	food[x].calories = 0;
+	pushFoodTail("-", tempname, flavor, "-", 0, price, size);
 	puts("\nSuccesfully added a new menu!");
 	x++;
 	getchar();
@@ -183,13 +204,21 @@ void add(){
 
 void history(){
 	cls();
+	// printf("| No | Name        | Price     | Topping    | Callories | Flavor       | size | Order Time              |\n");
+    // printf("---------------------------------------------------------------------------------------------------------\n");
+    // for(int i = 0;i < x;i++){
+    //     printf("| %-2d | %-11s | %-9d | %-10s | %-9.2lf | %-12s | %-4c | %d/%02d/%d %02d:%02d:%02d %s  |\n",i+1 ,  food[i].name, food[i].price, food[i].topping, food[i].calories, food[i].flavor, food[i].size, orderFood[i].year, orderFood[i].month, orderFood[i].day, orderFood[i].hours, orderFood[i].minute, orderFood[i].second, orderFood[i].time_stat);
+    // }
+	// puts("");
 	printf("| No | Name        | Price     | Topping    | Callories | Flavor       | size | Order Time              |\n");
     printf("---------------------------------------------------------------------------------------------------------\n");
-    for(int i = 0;i < x;i++){
-        printf("|%-3d | %-11s | %-9d | %-10s | %-9.2lf | %-12s | %-4c | %d/%02d/%d %02d:%02d:%02d %s  |\n",i+1 ,  food[i].name, food[i].price, food[i].topping, food[i].calories, food[i].flavor, food[i].size, orderFood[i].year, orderFood[i].month, orderFood[i].day, orderFood[i].hours, orderFood[i].minute, orderFood[i].second, orderFood[i].time_stat);
-    }
-	puts("");
-
+	historyCurr = historyHead;
+	int counter = 1;
+	while(historyCurr != NULL){
+		printf("| %-2d | %-11s | %-9d | %-10s | %-9.2lf | %-12s | %-4c | %d/%02d/%d %02d:%02d:%02d %s  |\n", counter ,  historyCurr->name, historyCurr->price, historyCurr->topping, historyCurr->calories, historyCurr->flavor, historyCurr->size, historyCurr->year, historyCurr->month, historyCurr->day, historyCurr->hours, historyCurr->minute, historyCurr->second, historyCurr->time_stat);
+		historyCurr = historyCurr->next;
+		counter++;
+	}
 	printf("Press Enter to continue.");
 	
 	getchar();
@@ -213,10 +242,13 @@ void order()
         cls();
         printf("| No | Name        | Price     | Topping    | Callories | Flavor       | size |\n");
         printf("-------------------------------------------------------------------------------\n");
-        for(int i = 0;i < x;i++)
-        {
-            printf("|%-3d | %-11s | %-9d | %-10s | %-9.2lf | %-12s | %-4c |\n",i+1 ,  food[i].name, food[i].price, food[i].topping, food[i].calories, food[i].flavor, food[i].size);
-        }
+		foodCurr = foodHead;
+		int index = 1;
+		while(foodCurr != NULL){
+            printf("|%-3d | %-11s | %-9d | %-10s | %-9.2lf | %-12s | %-4c |\n", index ,  foodCurr->name, foodCurr->price, foodCurr->topping, foodCurr->calories, foodCurr->flavor, foodCurr->size);
+			index++;
+			foodCurr = foodCurr->next;
+		}
         printf("Choose menu to order [1 - %d] : ", x);
         scanf("%d", &menuchoice);
 		getchar();
@@ -236,7 +268,7 @@ void order()
     	time(&now);
  
     	struct tm *local = localtime(&now);
-
+		char timeStat[5];
     	hours = local->tm_hour;          
     	minutes = local->tm_min;       
     	seconds = local->tm_sec;        
@@ -245,7 +277,7 @@ void order()
     	month = local->tm_mon + 1;        
     	year = local->tm_year + 1900;    
 
-    
+
 		if (hours < 12){
 			orderFood[ctrorder].day = day;
 			orderFood[ctrorder].month = month;
@@ -253,7 +285,7 @@ void order()
 			orderFood[ctrorder].second = seconds;
 			orderFood[ctrorder].minute = minutes;
 			orderFood[ctrorder].hours = hours;
-			strcpy(orderFood[ctrorder].time_stat, "AM");
+			strcpy(timeStat, "AM");
 		}else{ 
 			orderFood[ctrorder].day = day;
 			orderFood[ctrorder].month = month;
@@ -261,18 +293,22 @@ void order()
 			orderFood[ctrorder].second = seconds;
 			orderFood[ctrorder].minute = minutes;
 			orderFood[ctrorder].hours = hours - 12;
-			strcpy(orderFood[ctrorder].time_stat, "PM");
+			hours -= 12;
+			strcpy(timeStat, "PM");
 		}
-		
+		int time;
 		if(food[menuchoice-1].flavor[0] == '-'){
 			int random = rand()%41 + 50;
 			if(strcmp(food[menuchoice-1].topping, "honey") == 0 || strcmp(food[menuchoice-1].topping, "Honey") == 0){
+				time = random + 10;
 				orderFood[ctrorder].time = random + 10;
 			}
 			else if(strcmp(food[menuchoice-1].topping, "caramel") == 0 || strcmp(food[menuchoice-1].topping, "Caramel") == 0){
+				time = random + 15;
 				orderFood[ctrorder].time = random + 15;
 			}
 			else if(strcmp(food[menuchoice-1].topping, "syrup") == 0 || strcmp(food[menuchoice-1].topping, "Syrup") == 0){
+				time = random + 20;
 				orderFood[ctrorder].time = random + 20;
 			}
 		}
@@ -335,3 +371,51 @@ void menu(){
 	}	
 }
 
+void lowerCase(char str[]){
+	int counter = 0;
+	while(str[counter] != '\0'){
+		if(str[counter] <= 'Z' && str[counter] >= 'A'){
+			str[counter] = str[counter] - 'A' + 'a';
+		}
+		counter++;
+	}
+	return;
+}
+struct Food *createFood(char type[], char name[], char flavor[], char topping[], double calories, int price, char size){
+	struct Food *newFood = (struct Food*)malloc(sizeof(struct Food));
+	strcpy(newFood->type, type);
+	strcpy(newFood->name, name);
+	strcpy(newFood->flavor, flavor);
+	strcpy(newFood->topping, topping);
+	newFood->calories = calories;
+	newFood->price = price;
+	newFood->size = size;
+	newFood->next = NULL;
+	newFood->prev = NULL;
+	return newFood;
+}
+
+void pushFoodTail(char type[], char name[], char flavor[], char topping[], double calories, int price, char size){
+	struct Food* temp = createFood(type, name, flavor, topping, calories, price, size);
+	if(foodHead == NULL){
+		foodHead = foodTail = temp;
+	}
+	else{
+		temp->prev = foodTail;
+		foodTail->next = temp;
+		foodTail = temp;
+	}
+	return;
+}
+
+struct History *createHistory(char type[], char name[], char flavor[], char topping[], double calories, int price, char size, int time, int day, int month, int year, int hours, int minute, int second, char time_stat[]){
+	struct History *newHistory = (struct History*)malloc(sizeof(struct History));
+	strcpy(newHistory->name, name);
+    newHistory->price = price;
+    strcpy(newHistory->topping, topping);
+    newHistory->calories = calories;
+    strcpy(newHistory->flavor, flavor);
+    newHistory->size = size;
+	strcpy(newHistory->type, type);
+	return newHistory;
+}
